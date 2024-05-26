d62222e51e8b2c3e0e6da4378055aa35b2c577a2
dani1244 <121317384+dani1244@users.noreply.github.com>
2024-05-26 10:29:24 +0100

    one step more added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const nextBtnSec = document.querySelector(".next-1");
 const prevBtnThird = document.querySelector(".prev-2");
 const nextBtnThird = document.querySelector(".next-2");
 const prevBtnFourth = document.querySelector(".prev-3");
+const nextBtnFourth = document.querySelector(".next-3");
+const prevBtnFive = document.querySelector(".prev-4");
 const submitBtn = document.querySelector(".submit");
 const progressText = document.querySelectorAll(".step p");
 const progressCheck = document.querySelectorAll(".step .check");
@@ -28,6 +30,14 @@ nextBtnSec.addEventListener("click", function(event){
   current += 1;
 });
 nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnFourth.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-75%";
   bullet[current - 1].classList.add("active");
@@ -63,6 +73,14 @@ prevBtnThird.addEventListener("click", function(event){
   current -= 1;
 });
 prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFive.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
   bullet[current - 2].classList.remove("active");
