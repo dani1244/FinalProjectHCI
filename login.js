4ad4104f9546f9b6d2197c60106206ca93c4c28e
DanielCapitao <dcapitao@ua.pt>
2024-05-26 21:43:51 +0100

    login redirect to index page

--- login.js
+++ login.js
@@ -1,11 +1,25 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+    const signUpBtn = document.getElementById('signUpBtn');
+    const signInBtn = document.getElementById('signInBtn');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+    signUpBtn.addEventListener('click', () => {
+        // Perform any sign-up logic here
+        window.location.href = 'index.html';
+    });
+
+    signInBtn.addEventListener('click', () => {
+        // Perform any sign-in logic here
+        window.location.href = 'index.html';
+    });
 });
