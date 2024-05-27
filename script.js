59716d4600978a36b5ca51adae6b34802ff96561
dani1244 <121317384+dani1244@users.noreply.github.com>
2024-05-27 22:29:36 +0100

    FEAT: send submission

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const nextBtns = document.querySelectorAll('.next');
-//   const prevBtns = document.querySelectorAll('.prev');
-//   const form = document.querySelector('.form-outer form');
-//   const pages = document.querySelectorAll('.page');
-//   const bullets = document.querySelectorAll('.step .bullet');
-//   const progressChecks = document.querySelectorAll('.step .check');
-//   const progressTexts = document.querySelectorAll('.step p');
-  
-//   let current = 0;
+const submitBtn = document.querySelector('.submit');
 
-//   nextBtns.forEach((btn, index) => {
-//     btn.addEventListener('click', function () {
-//       bullets[current].classList.add('active');
-//       progressChecks[current].classList.add('active');
-//       progressTexts[current].classList.add('active');
-//       current += 1;
-//       form.style.transform = 'translateX(' + (-current * 25) + '%)';
-//       pages[current].scrollTop = 0;
-//     });
-//   });
 
-//   prevBtns.forEach((btn, index) => {
-//     btn.addEventListener('click', function () {
-//       bullets[current - 1].classList.remove('active');
-//       progressChecks[current - 1].classList.remove('active');
-//       progressTexts[current - 1].classList.remove('active');
-//       current -= 1;
-//       form.style.transform = 'translateX(' + (-current * 25) + '%)';
-//       pages[current].scrollTop = 0;
-//     });
-//   });
-// });
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault(); // Impede o envio do formulário
+  confirmSubmission();
+});
+
+function confirmSubmission() {
+  const userConfirmed = confirm("Are you sure you want to submit the form?");
+  if (userConfirmed) {
+    window.location.href = "./index.html";
+
+    form.submit();
+  }
+}
