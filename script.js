78b565a4215489dc30827d337a06c6002d131b1d
dani1244 <121317384+dani1244@users.noreply.github.com>
2024-05-27 22:02:28 +0100

    checkout refactoring

--- script.js
+++ script.js
@@ -1,92 +1,33 @@
-const slidePage = document.querySelector(".slide-page");
-const nextBtnFirst = document.querySelector(".firstNext");
-const prevBtnSec = document.querySelector(".prev-1");
-const nextBtnSec = document.querySelector(".next-1");
-const prevBtnThird = document.querySelector(".prev-2");
-const nextBtnThird = document.querySelector(".next-2");
-const prevBtnFourth = document.querySelector(".prev-3");
-const nextBtnFourth = document.querySelector(".next-3");
-const prevBtnFive = document.querySelector(".prev-4");
-const submitBtn = document.querySelector(".submit");
-const progressText = document.querySelectorAll(".step p");
-const progressCheck = document.querySelectorAll(".step .check");
-const bullet = document.querySelectorAll(".step .bullet");
-const form = document.querySelector('form');
-let current = 1;
+// document.addEventListener('DOMContentLoaded', function () {
+//   const nextBtns = document.querySelectorAll('.next');
+//   const prevBtns = document.querySelectorAll('.prev');
+//   const form = document.querySelector('.form-outer form');
+//   const pages = document.querySelectorAll('.page');
+//   const bullets = document.querySelectorAll('.step .bullet');
+//   const progressChecks = document.querySelectorAll('.step .check');
+//   const progressTexts = document.querySelectorAll('.step p');
+  
+//   let current = 0;
 
-nextBtnFirst.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-nextBtnSec.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-nextBtnThird.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-nextBtnFourth.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
+//   nextBtns.forEach((btn, index) => {
+//     btn.addEventListener('click', function () {
+//       bullets[current].classList.add('active');
+//       progressChecks[current].classList.add('active');
+//       progressTexts[current].classList.add('active');
+//       current += 1;
+//       form.style.transform = 'translateX(' + (-current * 25) + '%)';
+//       pages[current].scrollTop = 0;
+//     });
+//   });
 
-prevBtnSec.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "0%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
-prevBtnThird.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
-nextBtnFourth.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-100%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-
-prevBtnFive.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  setTimeout(function(){
-    alert("Your Form Successfully Signed up");
-    window.location.href = "index.html"; 
-  }, 800);
-});
+//   prevBtns.forEach((btn, index) => {
+//     btn.addEventListener('click', function () {
+//       bullets[current - 1].classList.remove('active');
+//       progressChecks[current - 1].classList.remove('active');
+//       progressTexts[current - 1].classList.remove('active');
+//       current -= 1;
+//       form.style.transform = 'translateX(' + (-current * 25) + '%)';
+//       pages[current].scrollTop = 0;
+//     });
+//   });
+// });
